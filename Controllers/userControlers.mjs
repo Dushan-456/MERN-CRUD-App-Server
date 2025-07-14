9cf7eb8f9e5a4b0e595dbd1e11b73e2ec5b20e32
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-14 22:33:16 +0530

    Update userControlers.mjs

--- Controllers/userControlers.mjs
+++ Controllers/userControlers.mjs
@@ -1,10 +1,25 @@
+import mongoose from "mongoose";
 import UserModel from "../Models/UserModel.mjs";
+import { errorCreate } from "../Utils/error-creator.mjs";
+import { matchedData, validationResult } from "express-validator";
 
 class UserController{
 
     // Create new user------------------------------------------------------------------------------------------------------------------------------
 createNewUser = async(req,res)=>{
-    const {name,gmail,age,address} = req.body;
+    const error = validationResult(req);
+    const creatingError = errorCreate(error.array())
+    if(error.array().length){
+        console.log(creatingError);
+        
+            return res.status(400).json({
+                    msg : "error",
+                    error : creatingError,
+                    data : null,
+                });
+    }
+
+    const {name,gmail,age,address} =  matchedData(req);
     try {
         const newUser = await UserModel.create({name,gmail,age,address})
          return res.status(201).json({
@@ -19,7 +34,7 @@ createNewUser = async(req,res)=>{
            if(error.code===11000){
               return res.status(409).json({
             msg : "error",
-            error : "This Username Already registerd",
+            error : "This Email Already registerd",
             data : null,
         })
     }
@@ -53,24 +68,49 @@ showAllUsers = async(req,res)=>{
 
 //Get  Users by ID------------------------------------------------------------------------------------------------------------------------------
 
-getUserById = async(req,res)=>{
-    try {
-        const user = await UserModel.findById(req.params.id)
-           return  res.status(200).json({
-                    msg : " Users by ID",
-                    data : user,
-        }); 
-        
-    } catch (error) {
-        res.status(500).json({
-                    msg : "error",
-                    error : "Internal Server Error",
-        });    
-        
+
+getUserById = async (req, res) => {
+    const id = req.params.id;
+
+    if (id && id !== "") {
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({
+                msg: "error",
+                error: "Invalid ID format",
+                data: null,
+            });
+        }
+
+        try {
+            const user = await UserModel.findById(id);
+            if (user !== null) {
+                return res.status(200).json({
+                    msg: "user data",
+                    data: user,
+                });
+            }
+
+            return res.status(404).json({
+                msg: "error",
+                error: "User not found",
+                data: null,
+            });
+
+        } catch (error) {
+            return res.status(500).json({
+                msg: "error",
+                error: "Internal Server Error",
+            });
+        }
+    } else {
+        return res.status(400).json({
+            msg: "error",
+            error: "Missing ID",
+            data: null,
+        });
     }
 }
 
-    
 }
 
 export default new UserController();
