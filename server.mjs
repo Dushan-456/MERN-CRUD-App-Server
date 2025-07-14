ac6b91afc53f9c7f061ef64fe06676028bbbc1a6
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-14 21:28:07 +0530

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -4,7 +4,7 @@ import dbConnect from './src/db/config.mjs';
 import rootRouter from './Routes/index.mjs';
 
 
-
+// MONGO_URL = mongodb+srv://dushan:7QDYKoAxSKoRc05J@cluster0.4l8hnzf.mongodb.net/express_learnig_db?retryWrites=true&w=majority&appName=Cluster0
 
 const server = express()
 const PORT = process.env.PORT || 5001
