c705830cdeb120ed3208bfaca019131943cfcf13
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-16 18:06:53 +0530

    Update UserModel.mjs

--- Models/UserModel.mjs
+++ Models/UserModel.mjs
@@ -1,54 +1,57 @@
 import { model, Schema } from "mongoose";
 
-const userSchema = new Schema({
-        first_name:{
-            type:String,  // data Type
-            required:true, // Validate
-        },  
-        last_name:{
-            type:String,  // data Type
-            required:true, // Validate
-        },  
-        dob:{
-            type:String,  // data Type
-            required:true, // Validate
-        },  
-        designation:{
-            type:String,  // data Type
-            required:true, // Validate
-        },  
-        mobile:{
-            type:String,  // data Type
-            required:true, // Validate
-        },  
-        gmail:{
-            type:String,  // data Type
-            required:true, // Validate
-             unique:true
-        },          
-        age:{
-            type:Number,  // data Type
-            required:true, // Validate
-        },  
-        gender:{
-            type:String,  // data Type
-            required:true, // Validate
-        },  
-        fb_profile:{
-            type:String,  // data Type
-            required:true, // Validate
-        },  
-        profilePicture:{
-            type:String,  // data Type
-        },  
-        address:{
-            type:String,  // data Type
-            required:true, // Validate
-        }
-},{
-    timestamps:true,
-})
+const userSchema = new Schema(
+   {
+      first_name: {
+         type: String, // data Type
+         required: true, // Validate
+      },
+      last_name: {
+         type: String, // data Type
+         required: true, // Validate
+      },
+      dob: {
+         type: String, // data Type
+         required: true, // Validate
+      },
+      designation: {
+         type: String, // data Type
+         required: true, // Validate
+      },
+      mobile: {
+         type: String, // data Type
+         required: true, // Validate
+      },
+      gmail: {
+         type: String, // data Type
+         required: true, // Validate
+         unique: true,
+      },
+      age: {
+         type: Number, // data Type
+         required: true, // Validate
+      },
+      gender: {
+         type: String, // data Type
+         required: true, // Validate
+      },
+      fb_profile: {
+         type: String, // data Type
+         required: true, // Validate
+      },
+      profilePicture: {
+         type: String, // data Type
+      },
+      address: {
+         type: String, // data Type
+         required: true, // Validate
+      },
+   },
+   {
+      timestamps: true,
+   }
+);
 
-const UserModel = model("CRUDUsers",userSchema)
+const UserModel = model("CRUDUsers", userSchema);
 
 export default UserModel;
