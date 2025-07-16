1ca5567fec32f52828fd82e777939e2304dc0e85
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-16 15:49:24 +0530

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -5,7 +5,6 @@ import rootRouter from './Routes/index.mjs';
 import cors from 'cors'
 
 
-// MONGO_URL = mongodb+srv://dushan:7QDYKoAxSKoRc05J@cluster0.4l8hnzf.mongodb.net/express_learnig_db?retryWrites=true&w=majority&appName=Cluster0
 
 const server = express()
 const PORT = process.env.PORT || 5001
@@ -14,6 +13,10 @@ server.use(express.json())
 server.use(cors());
 //ROUTE CONNECT 
 server.use('/api/v1/',rootRouter);
+
+server.use('/uploads', express.static('uploads'));
+
+
 
 dbConnect.then(()=>{
     console.log("DB Connected...... :)");
